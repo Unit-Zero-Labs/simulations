b0036a6e59a31ff51d44636d5ea01650896ce6d8
Jack Forlines <jackforlines23@gmail.com>
2024-07-03 19:18:06 -0700

    heroku fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,20 @@ from flask import Flask
 from config import Config
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, 
+                static_folder='static', 
+                template_folder='templates')
     app.config.from_object(config_class)
 
-    from app.routes import main, lending, stable_pool, non_stable_pool
-    app.register_blueprint(main.bp)
-    app.register_blueprint(lending.bp)
-    app.register_blueprint(stable_pool.bp)
-    app.register_blueprint(non_stable_pool.bp)
+    # import & register blueprints
+    from app.routes.main import main_bp
+    from app.routes.lending import lending_bp
+    from app.routes.stable_pool import stable_pool_bp
+    from app.routes.non_stable_pool import non_stable_pool_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(lending_bp)
+    app.register_blueprint(stable_pool_bp)
+    app.register_blueprint(non_stable_pool_bp)
 
     return app

8fb23c9cda65f5c4490369fd56c56cd7ccd3981c
Jack Forlines <jackforlines23@gmail.com>
2024-07-15 16:31:53 -0700

    blueprint fix

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,8 @@ def create_app(config_class=Config):
     from app.routes.non_stable_pool import non_stable_pool_bp
     from app.routes.tokenomics import tokenomics_bp    
     from app.routes.fund_raising import fund_raising_bp 
+    from app.routes.lp_sim import lp_sim_bp 
+
 
     app.register_blueprint(main_bp)
     app.register_blueprint(lending_bp)
@@ -21,5 +23,6 @@ def create_app(config_class=Config):
     app.register_blueprint(non_stable_pool_bp)
     app.register_blueprint(tokenomics_bp)
     app.register_blueprint(fund_raising_bp)
+    app.register_blueprint(lp_sim_bp)
 
     return app

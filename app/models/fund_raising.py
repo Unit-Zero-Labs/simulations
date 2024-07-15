6ee105745072009ce4b1cb0c564f1c91682a6233
Jack Forlines <jackforlines23@gmail.com>
2024-07-15 15:30:10 -0700

    hail mary

--- app/models/fund_raising.py
+++ app/models/fund_raising.py
@@ -5,22 +5,26 @@ import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
 class FundRaising:
-    def __init__(self, allocation_data, vesting_data, airdrop_module):
+    def __init__(self, allocation_data, vesting_data, airdrop_module, initial_total_supply, public_sale_valuation):
         self.allocation_data = pd.DataFrame(allocation_data)
         self.vesting_data = pd.DataFrame(vesting_data)
         self.airdrop_module = airdrop_module
+        self.initial_total_supply = initial_total_supply
+        self.public_sale_valuation = public_sale_valuation
 
     def generate_pie_chart(self):
+        self.allocation_data['tokens'] = self.allocation_data['percentage'] * self.initial_total_supply / 100
         fig = go.Figure(data=[go.Pie(
             labels=self.allocation_data['allocation'],
-            values=self.allocation_data['percentage'],
-            textinfo='label+percent',
-            insidetextorientation='radial'
+            values=self.allocation_data['tokens'],
+            textinfo='label+value',
+            insidetextorientation='radial',
+            hovertemplate='%{label}<br>Tokens: %{value:,.0f}<br>Percentage: %{percent}'
         )])
         
         fig.update_layout(
-            title_text='Token Allocation',
-            showlegend=False
+            title_text=f'Token Allocation (Total Supply: {self.initial_total_supply:,})',
+            showlegend=True
         )
         
         return fig
@@ -58,6 +62,9 @@ class FundRaising:
         
         vesting_data = vesting_data.fillna(0).cumsum()
         
+        # cap vesting data at initial total supply
+        vesting_data = vesting_data.clip(upper=self.initial_total_supply)
+        
         fig = go.Figure()
         for column in vesting_data.columns:
             fig.add_trace(go.Scatter(
@@ -68,12 +75,31 @@ class FundRaising:
                 stackgroup='one'
             ))
         
+        # add line for total supply
+        fig.add_trace(go.Scatter(
+            x=[vesting_data.index[0], vesting_data.index[-1]],
+            y=[self.initial_total_supply, self.initial_total_supply],
+            mode='lines',
+            name='Total Supply',
+            line=dict(color='red', dash='dash')
+        ))
+        
+        public_sale_tokens = self.vesting_data.loc[self.vesting_data['allocation'] == 'Public Sale', 'tokens'].values[0]
+        token_price = self.public_sale_valuation / public_sale_tokens
+        implied_market_cap = token_price * self.initial_total_supply
+        
         fig.update_layout(
-            title_text='Token Vesting Schedule',
+            title_text=f'Token Vesting Schedule<br>Implied Market Cap: ${implied_market_cap:,.2f}<br>Token Price: ${token_price:.4f}',
             xaxis_title='Date',
             yaxis_title='Vested Tokens',
-            legend_title='Allocation'
+            legend_title='Allocation',
+            hovermode='x unified'
         )
         
         return fig
 
+    def generate_charts(self):
+        return {
+            'pie_chart': self.generate_pie_chart().to_json(),
+            'vesting_chart': self.generate_vesting_chart().to_json()
+        }
